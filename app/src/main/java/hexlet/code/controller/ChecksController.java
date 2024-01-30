a999f14fe3ca69a4db2b1ab062ab4a9d7052cb58
blonde2029 <mr_blonde@rambler.ru>
2024-01-30 10:08:11 +0700

    fixed checks for url with null host

--- app/src/main/java/hexlet/code/controller/ChecksController.java
+++ app/src/main/java/hexlet/code/controller/ChecksController.java
@@ -4,12 +4,15 @@ import hexlet.code.dto.UrlPage;
 import hexlet.code.model.UrlCheck;
 import hexlet.code.repository.ChecksRepository;
 import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.validation.ValidationException;
 import kong.unirest.Unirest;
 import org.jsoup.Jsoup;
 
+import java.net.URI;
+import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Collections;
@@ -23,25 +26,31 @@ public class ChecksController {
         var url = UrlsRepository.find(urlId).orElseThrow(() -> new NotFoundResponse("Entity with id "
                 + urlId + " not found"));
         var urlPath = url.getName();
-        var response = Unirest.get(urlPath).asString();
-        var body = response.getBody();
-        var responsePage = Jsoup.parse(body);
-        var statusCode = response.getStatus();
-        var title = responsePage.getElementsByTag("title").text();
-        var h1 = responsePage.getElementsByTag("h1").text();
-        String description = responsePage.select("meta[name=description]").attr("content");
-        var createdAt = new Timestamp(new Date().getTime());
+        var uri = URI.create(urlPath);
+        if (uri.getHost() == null) {
+            ctx.sessionAttribute("flash", "Некорректный адрес");
+            ctx.redirect(NamedRoutes.urlPath(url.getId()));
+        } else {
+            var response = Unirest.get(urlPath).asString();
+            var body = response.getBody();
+            var responsePage = Jsoup.parse(body);
+            var statusCode = response.getStatus();
+            var title = responsePage.getElementsByTag("title").text();
+            var h1 = responsePage.getElementsByTag("h1").text();
+            String description = responsePage.select("meta[name=description]").attr("content");
+            var createdAt = new Timestamp(new Date().getTime());
 
-        var urlCheck = new UrlCheck(createdAt, title, h1, description, statusCode, urlId);
+            var urlCheck = new UrlCheck(createdAt, title, h1, description, statusCode, urlId);
 
-        try {
-            ChecksRepository.save(urlCheck);
-            ctx.sessionAttribute("flash", "Страница успешно проверена");
-            ctx.redirect("/urls/" + url.getId());
-        } catch (ValidationException e) {
-            var checks = ChecksRepository.getEntitiesById(urlId);
-            var page = new UrlPage(url, checks);
-            ctx.render("pages/urlPage.jte", Collections.singletonMap("page", page));
+            try {
+                ChecksRepository.save(urlCheck);
+                ctx.sessionAttribute("flash", "Страница успешно проверена");
+                ctx.redirect("/urls/" + url.getId());
+            } catch (ValidationException e) {
+                var checks = ChecksRepository.getEntitiesById(urlId);
+                var page = new UrlPage(url, checks);
+                ctx.render("pages/urlPage.jte", Collections.singletonMap("page", page));
+            }
         }
     }
 
